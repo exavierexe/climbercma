ce4de729cfb63aa222d19edacbb6e1cfc9df7aa4
Exavier Lattimore <exavierexe@gmail.com>
2025-05-09 22:06:50 +1200

    img

--- src/app/valuation-step2/page.tsx
+++ src/app/valuation-step2/page.tsx
@@ -1,7 +1,6 @@
 "use client";
 import React, { useState, useEffect } from "react";
 import { useRouter } from "next/navigation";
-import Image from "next/image";
 
 export default function ValuationStep2() {
   const router = useRouter();
